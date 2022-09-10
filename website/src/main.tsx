03e9310dd7e53d8ed91cccf390429a87f27f3a23
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-10 23:42:01 +0900

    Create website and deploy it to firebase (#4)
    * Create website by vite
    
    * Install node modules
    
    * Add firebase configure
    
    * Add firebase tools
    
    * Configure firebase prj and hosting workflow
    
    * Add label
    
    * Configure cache
    
    * Configure working-directory to deploy
    
    * Move firebase config to root dir

---
+++ website/src/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBamdEeb3h3eq0NlsqH-TGnMiMabv4L2-E",
+  authDomain: "umie-prj.firebaseapp.com",
+  projectId: "umie-prj",
+  storageBucket: "umie-prj.appspot.com",
+  messagingSenderId: "475480462609",
+  appId: "1:475480462609:web:56e828b10272b142c7a8b0",
+  measurementId: "G-NMHBTCMCSK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
