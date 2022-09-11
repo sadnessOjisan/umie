78cc3b15f5caccdb881cbc1afd0d0dfc0ccce0e5
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-11 21:00:41 +0900

    Exec mosaic by using image-rs (#14)
    * Remove my image mod
    
    * Use image crate

--- packages/core/src/lib.rs
+++ packages/core/src/lib.rs
@@ -1,15 +1,11 @@
-pub mod image;
+use image::{EncodableLayout, ImageBuffer, Rgba};
 
-use std::vec;
-
-use image::Image;
-
-pub fn mosaic(buf: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
-    let img = Image { width, data: buf };
-    let block_size: u32 = 8;
-    let mut vec = vec![];
+pub fn exec(buf: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
+    let mut img: ImageBuffer<Rgba<u8>, Vec<_>> = ImageBuffer::from_raw(width, height, buf).unwrap();
+    let width = img.width();
+    let height = img.height();
+    let block_size: u32 = 32;
     for h in (0..height).step_by(block_size as usize) {
-        let mut row = vec![];
         for w in (0..width).step_by(block_size as usize) {
             let mut r_sum: u32 = 0;
             let mut g_sum: u32 = 0;
@@ -26,41 +22,31 @@ pub fn mosaic(buf: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
                     if width <= w + x as u32 {
                         break;
                     }
-                    r_sum += *img.getPixel(w + x, h + y).0 as u32;
-                    g_sum += *img.getPixel(w + x, h + y).1 as u32;
-                    b_sum += *img.getPixel(w + x, h + y).2 as u32;
-                    a_sum += *img.getPixel(w + x, h + y).3 as u32;
+                    r_sum += img.get_pixel(w + x, h + y).0[0] as u32;
+                    g_sum += img.get_pixel(w + x, h + y).0[1] as u32;
+                    b_sum += img.get_pixel(w + x, h + y).0[2] as u32;
+                    a_sum += img.get_pixel(w + x, h + y).0[3] as u32;
                     safe_area_x = x + 1;
                 }
             }
 
-            println!("{}", safe_area_x);
-
-            // for y in 0..safe_area_y {
-            for x in 0..safe_area_x {
-                row.push((r_sum / (block_size * block_size)) as u8);
-                row.push((g_sum / (block_size * block_size)) as u8);
-                row.push((b_sum / (block_size * block_size)) as u8);
-                row.push((a_sum / (block_size * block_size)) as u8);
-
-                // img.put_pixel(
-                //     w + x,
-                //     h + y,
-                //     Rgba([
-                //         (r_sum / (block_size * block_size)) as u8,
-                //         (g_sum / (block_size * block_size)) as u8,
-                //         (b_sum / (block_size * block_size)) as u8,
-                //         (a_sum / (block_size * block_size)) as u8,
-                //     ]),
-                // )
+            for y in 0..safe_area_y {
+                for x in 0..safe_area_x {
+                    img.put_pixel(
+                        w + x,
+                        h + y,
+                        Rgba([
+                            (r_sum / (block_size * block_size)) as u8,
+                            (g_sum / (block_size * block_size)) as u8,
+                            (b_sum / (block_size * block_size)) as u8,
+                            (a_sum / (block_size * block_size)) as u8,
+                        ]),
+                    )
+                }
             }
-            // }
-        }
-        for x in 0..block_size {
-            vec.push(row.clone());
         }
     }
     // img.as_raw().to_vec()
     // let _ = img.save("./umie/src/neww.jpeg");
-    vec.into_iter().flatten().collect()
+    img.as_bytes().to_vec()
 }
