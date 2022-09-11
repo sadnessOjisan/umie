b7501b67109a297388c6e0768925fc2da934a0ce
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-11 21:41:27 +0900

    Load image and save image via cli

--- packages/cli/src/main.rs
+++ packages/cli/src/main.rs
@@ -1,3 +1,18 @@
+use core::exec;
+use image::{io::Reader as ImageReader, ImageBuffer, ImageFormat, RgbImage};
+use std::{
+    fs::{self, File},
+    io::BufReader,
+    path::Path,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("./packages/cli/src/test.png");
+    let img = ImageReader::open(path).unwrap().decode().unwrap();
+    let width = img.width();
+    let height = img.height();
+    let vec = img.as_bytes().to_vec();
+    let converted = exec(vec, width, height);
+    let img = RgbImage::from_vec(width, height, converted).unwrap();
+    img.save("./packages/cli/src/result.png");
 }
