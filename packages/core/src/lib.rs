4cc4d5da859c235320e6c7f79b066fbbe6609916
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-11 14:19:29 +0900

    Add mosaic module

--- packages/core/src/lib.rs
+++ packages/core/src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod image;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use image::Image;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn mosaic(buf: Vec<u32>, width: u32, height: u32) -> Vec<u32> {
+    let img = Image { width, data: buf };
+    let block_size: u32 = 32;
+    for h in (0..height).step_by(block_size as usize) {
+        for w in (0..width).step_by(block_size as usize) {
+            let mut r_sum: u32 = 0;
+            let mut g_sum: u32 = 0;
+            let mut b_sum: u32 = 0;
+            let mut a_sum: u32 = 0;
+            let mut safe_area_x = 0;
+            let mut safe_area_y = 0;
+            for y in 0..block_size as u32 {
+                if height <= h + y as u32 {
+                    break;
+                }
+                safe_area_y = y as u32 + 1;
+                for x in 0..block_size as u32 {
+                    if width <= w + x as u32 {
+                        break;
+                    }
+                    r_sum += *img.getPixel(w + x, h + y).0;
+                    g_sum += *img.getPixel(w + x, h + y).1;
+                    b_sum += *img.getPixel(w + x, h + y).2;
+                    a_sum += *img.getPixel(w + x, h + y).3;
+                    safe_area_x = x + 1;
+                }
+            }
+
+            // for y in 0..safe_area_y {
+            //     for x in 0..safe_area_x {
+            //         img.put_pixel(
+            //             w + x,
+            //             h + y,
+            //             Rgba([
+            //                 (r_sum / (block_size * block_size)) as u8,
+            //                 (g_sum / (block_size * block_size)) as u8,
+            //                 (b_sum / (block_size * block_size)) as u8,
+            //                 (a_sum / (block_size * block_size)) as u8,
+            //             ]),
+            //         )
+            //     }
+            // }
+        }
     }
+    // img.as_raw().to_vec()
+    // let _ = img.save("./umie/src/neww.jpeg");
+    img.data
 }
