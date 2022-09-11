456f3809e56f29d5c57d87526b895f43aaff121d
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-11 21:47:25 +0900

    Load image from vec

--- packages/cli/src/main.rs
+++ packages/cli/src/main.rs
@@ -1,10 +1,6 @@
 use core::exec;
-use image::{io::Reader as ImageReader, ImageBuffer, ImageFormat, RgbImage};
-use std::{
-    fs::{self, File},
-    io::BufReader,
-    path::Path,
-};
+use image::{io::Reader as ImageReader, RgbaImage};
+use std::path::Path;
 
 fn main() {
     let path = Path::new("./packages/cli/src/test.png");
@@ -13,6 +9,6 @@ fn main() {
     let height = img.height();
     let vec = img.as_bytes().to_vec();
     let converted = exec(vec, width, height);
-    let img = RgbImage::from_vec(width, height, converted).unwrap();
-    img.save("./packages/cli/src/result.png");
+    let img = RgbaImage::from_vec(width, height, converted).unwrap();
+    let _ = img.save("./packages/cli/src/result.png");
 }
