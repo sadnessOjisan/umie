268beab11e71a6fe0396432186e4dc42047c7801
sadnessOjisan <sadness.ojisan@gmail.com>
2022-09-11 20:52:28 +0900

    Use image crate

--- packages/wasm/src/lib.rs
+++ packages/wasm/src/lib.rs
@@ -1,8 +1,8 @@
-use core::mosaic;
+use core::exec;
 
 use wasm_bindgen::{prelude::wasm_bindgen, Clamped};
 
 #[wasm_bindgen]
 pub fn exec_mosaic(buf: Clamped<Vec<u8>>, width: u32, height: u32) -> Vec<u8> {
-    mosaic(buf.0, width, height)
+    exec(buf.0, width, height)
 }
